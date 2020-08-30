8a6579f0edd0c864e9bb155cb16691af394ce867
Jeffry joseph <48963703+jeffry01@users.noreply.github.com>
2020-08-30 23:21:37 +0530

    version2

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 
+    
     (function ($) {
     "use strict"; // Start of use strict
-   
+  
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
